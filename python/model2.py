722e4349839aa9bd5f695d4e6c21a3cd0e798c11
pinakm9 <pinakm9@gmail.com>
2020-06-14 15:54:45 +0530

    upf_ex 2.0

---
+++ python/model2.py
@@ -0,0 +1,8 @@
+import simulate as sm
+import filter as fl
+import numpy as np
+import scipy
+
+"""
+A 1D non-linear problem
+"""
--- python/utility.py
+++ python/utility.py
@@ -11,7 +11,7 @@ def timer(func):
 		start = time()
 		val = func(*args,**kwargs)
 		end = time()
-		print('Time taken by function {} is {} seconds'.format(func.__name__, end-start))
+		print('Time taken by {} is {} seconds'.format(func.__name__, end-start))
 		return val
 	return new_func
 
